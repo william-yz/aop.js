d36c9c681d44597edb5d5c5184a468fcfad7cce6
William Yang <william.yang@oocl.com>
2016-08-26 17:38:46 +0800

    node-aop

---
+++ index.js
@@ -0,0 +1,41 @@
+function aop(f) {
+  return new aop.prototype.init(f)
+}
+aop.prototype = {
+    init: function(f) {
+      this.f = f
+      this._before = []
+      this._after = []
+      return this
+    },
+    before : function (before) {
+      if(this._after.length > 0) {
+        throw new Error(`'before()' function must be called before 'after()'`)
+      }
+      this._before.push(before)
+      return this
+    },
+    after: function (after) {
+      this._after.push(after)
+      return this
+    },
+    end: function (f) {
+      f = f || function () {}
+      function _chain(functionArray, index, chain) {
+        if (functionArray.length === 0 || index === -1) return chain
+        return _chain(functionArray, index - 1, functionArray[index].bind(this, chain))
+      }
+      return _chain(this._before, this._before.length - 1, function () {
+        const result = this.f()
+        const afterFunction = _chain(this._after, this._after.length - 1, f)
+        if (result instanceof Promise) {
+          result.then(afterFunction)
+        }else {
+          afterFunction()
+        }
+      }.bind(this))
+    }
+}
+aop.prototype.init.prototype = aop.prototype;
+
+module.exports = aop
---
+++ readme.md
@@ -0,0 +1,48 @@
+# node-aop  lib
+
+```javascript
+var originFunction = function () {
+  return new Promise(function (resolve, reject) {
+    console.log('in origin function')
+    setTimeout(function () {
+      resolve('pass to after')
+    }, 1000)
+  })
+}
+
+var aopi = aop(originFunction)
+.before(function a(next) {
+  console.log('1')
+  next('pass to next')
+})
+.before(function b(next,a) {
+  console.log('here in next :' + a)
+  console.log('2')
+  next()
+})
+.before(function (next) {
+  console.log('after 2 s')
+  setTimeout(next, 2000)
+})
+.before(function (next) {
+  console.log('after 1 s')
+  setTimeout(next, 1000)
+})
+.before(function c(next) {
+  console.log('3')
+  next()
+})
+.after(function d(next, b) {
+  console.log(b)
+  next()
+})
+.after(function d(next) {
+  console.log('after 3 s')
+  setTimeout(next, 3000)
+})
+.end(function () {
+  console.log('end')
+})
+
+aopi()
+```
